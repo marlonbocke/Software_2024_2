73f2ec1ff89a5c9e8716b3423b94b5e965625f5c
marlonbocke <149473222+marlonbocke@users.noreply.github.com>
2024-10-23 12:08:10 +0200

    updated a-search-algorithm

--- Krankenhaus2024/src/package01/Interface.java
+++ Krankenhaus2024/src/package01/Interface.java
@@ -110,221 +110,66 @@ public class Interface {
         frame.getContentPane().add(textfield_destination_room);
     }
 
+    
+    
+    
+    
     public void calulate() {
 
-    	int[][] coordinates_range = new int[65][2];
-
-        coordinates_range[0][0] = 0; 
-        coordinates_range[0][1] = 0; 
-        coordinates_range[1][0] = 0; 
-        coordinates_range[1][1] = 276; 
-
-        
-        
-        coordinates_range[2][0] = 0; 
-        coordinates_range[2][1] = 276; 
-        coordinates_range[3][0] = 188; 
-        coordinates_range[3][1] = 276; 
-
-
-        
-        coordinates_range[4][0] = 188; 
-        coordinates_range[4][1] = 276; 
-        coordinates_range[5][0] = 188; 
-        coordinates_range[5][1] = 458; 
-
-        
-        coordinates_range[6][0] = 188; //X
-        coordinates_range[6][1] = 458; //Y 
-        coordinates_range[7][0] = 497; 
-        coordinates_range[7][1] = 458; 
-
-        
-        coordinates_range[8][0] = 497; 
-        coordinates_range[8][1] = 458; 
-        coordinates_range[9][0] = 497; 
-        coordinates_range[9][1] = 276; 
+    	
+   int[] startNode = {100, 200};  // coordinates of the start node
+   int[] goalNode = {500, 400};   // coordinates of the destination node
 	
+  // double distance = manhattanDistance(startNode, goalNode);
+    	
+ }
+    private static double manhattanDistance(int[] startNode, int[] goalNode) {
+    	
+    	
+    	int x12 = startNode[0];
+        int y12 = startNode[1];
+        int x13 = goalNode[0];
+        int y13 = goalNode[1];
+
         
-        coordinates_range[10][0] = 497; 
-       	coordinates_range[10][1] = 276; 
-       	coordinates_range[11][0] = 641; 
-       	coordinates_range[11][1] = 276;
-       	
-       	
-       	coordinates_range[12][0] = 641; 
-       	coordinates_range[12][1] = 276;
-       	coordinates_range[13][0] = 641; 
-       	coordinates_range[13][1] = 0;
-       	
-       	coordinates_range[14][0] = 641; 
-       	coordinates_range[15][1] = 0;
-       	coordinates_range[16][0] = 379; 
-       	coordinates_range[16][1] = 0;
-       	
-       	coordinates_range[16][0] = 379; 
-       	coordinates_range[16][1] = 0;
-       	coordinates_range[17][0] = 163; 
-       	coordinates_range[17][1] = 379;
-	
+        int result=(int) Math.sqrt(Math.pow(x13 - x12, 2) + Math.pow(y13 - y12, 2));
+        	
+        	
+        
+        return result;
+       
+    	
+			}
+
     
-       	coordinates_range[16][0] = 379; 
-       	coordinates_range[16][1] = 0;
-       	coordinates_range[17][0] = 379; 
-       	coordinates_range[17][1] = 163;
-	
-       	
-    	coordinates_range[18][0] = 379; 
-       	coordinates_range[18][1] = 163;
-       	coordinates_range[19][0] = 269; 
-       	coordinates_range[19][1] = 163;
-       	
-       	
-    	coordinates_range[20][0] = 269; 
-       	coordinates_range[20][1] = 163;
-       	coordinates_range[21][0] = 269; 
-       	coordinates_range[21][1] = 0;
-       	
-       	
-       	coordinates_range[22][0] = 269; 
-       	coordinates_range[22][1] = 0;
-       	coordinates_range[23][0] = 0; 
-       	coordinates_range[23][1] = 0;
-       	
-       	//------------------------------------------------------------^^ define out_walls
-       	
-       	
-    	coordinates_range[24][0] = 10; 
-       	coordinates_range[24][1] = 0;
-       	coordinates_range[25][0] = 10; 
-       	coordinates_range[25][1] = 297;
-       	
-       	
-     	coordinates_range[26][0] = 64; 
-       	coordinates_range[26][1] = 61;
-       	coordinates_range[27][0] = 64; 
-       	coordinates_range[27][1] = 264;
-       	
-       	
-       	coordinates_range[28][0] = 80; 
-       	coordinates_range[28][1] = 61;
-       	coordinates_range[29][0] = 80; 
-       	coordinates_range[29][1] = 222;
-       	
-        
-       	coordinates_range[30][0] = 80; 
-       	coordinates_range[30][1] = 61;
-       	coordinates_range[31][0] = 80; 
-       	coordinates_range[31][1] = 222;
-       
-       	
-    	coordinates_range[32][0] = 80; 
-       	coordinates_range[32][1] = 61;
-       	coordinates_range[33][0] = 199; 
-       	coordinates_range[33][1] = 61;
-       
-        
-     	coordinates_range[34][0] = 196; 
-       	coordinates_range[34][1] = 61;
-       	coordinates_range[35][0] = 196; 
-       	coordinates_range[35][1] = 222;
-       
-       	
-       	coordinates_range[36][0] = 214; 
-       	coordinates_range[36][1] = 222;
-       	coordinates_range[37][0] = 214; 
-       	coordinates_range[37][1] = 222;
-       
-        
-    	coordinates_range[38][0] = 65; 
-       	coordinates_range[38][1] = 238;
-       	coordinates_range[39][0] = 584; 
-       	coordinates_range[39][1] = 238;
-       
-       	
-       	coordinates_range[40][0] = 65; 
-       	coordinates_range[40][1] = 218;
-       	coordinates_range[41][0] = 584; 
-       	coordinates_range[41][1] = 218;
-        
-        
-       	coordinates_range[42][0] = 246; 
-       	coordinates_range[42][1] = 238;
-       	coordinates_range[43][0] = 246; 
-       	coordinates_range[43][1] = 293;
-        
-       	
-       	coordinates_range[44][0] = 269; 
-       	coordinates_range[44][1] = 238;
-       	coordinates_range[45][0] = 269; 
-       	coordinates_range[45][1] = 293;
-        
-       	
-       	coordinates_range[46][0] = 190; 
-       	coordinates_range[46][1] = 409;
-       	coordinates_range[47][0] = 493; 
-       	coordinates_range[47][1] = 409 ;
-        
-       	
-       	coordinates_range[48][0] = 190; 
-       	coordinates_range[48][1] = 391;
-       	coordinates_range[49][0] = 190; 
-       	coordinates_range[49][1] = 391;
-        
-       	
-     	coordinates_range[50][0] = 429; 
-       	coordinates_range[50][1] = 238;
-       	coordinates_range[51][0] = 429; 
-       	coordinates_range[51][1] = 408;
-       	
-       	
-       	coordinates_range[52][0] = 443; 
-       	coordinates_range[52][1] = 238;
-       	coordinates_range[53][0] = 443; 
-       	coordinates_range[53][1] = 408;
-       	
-       	
-       	coordinates_range[54][0] = 434; 
-       	coordinates_range[54][1] = 216;
-       	coordinates_range[55][0] = 434; 
-       	coordinates_range[55][1] = 41;
-       	
-       	
-       	coordinates_range[56][0] = 450; 
-       	coordinates_range[56][1] = 216;
-       	coordinates_range[57][0] = 450; 
-       	coordinates_range[57][1] = 41;
-       	
-       	
-       	coordinates_range[58][0] = 377; 
-       	coordinates_range[58][1] = 58;
-       	coordinates_range[59][0] = 640; 
-       	coordinates_range[59][1] = 58;
-       	
-       	
-       	coordinates_range[60][0] = 377; 
-       	coordinates_range[60][1] = 41;
-       	coordinates_range[61][0] = 640; 
-       	coordinates_range[61][1] = 41;
-       	
-       	
-       	coordinates_range[62][0] = 562; 
-       	coordinates_range[62][1] = 59;
-       	coordinates_range[63][0] = 562;
-       	coordinates_range[63][1] = 221;
-       	
-       	
-       	coordinates_range[64][0] = 585; 
-       	coordinates_range[64][1] = 59;
-       	coordinates_range[65][0] = 585; 
-       	coordinates_range[65][1] = 221;
-       	
-       	
-       	//----------------------------------^^define corridors
-       	
+    
+    public static void distance(String[] arg) {
+        // Array of node coordinates
+        int[][] nodes = {
 
-//        for (int i = 0; i < coordinates.length; i++) {
-//            System.out.println("Koordinate " + (i + 1) + ": (x = " + coordinates[i][0] + ", y = " + coordinates[i][1] + ")");
-//        }
+            {64, 47}, {64, 226}, {204, 47}, {204, 226}, {252, 226},
+            {252, 395}, {436, 395}, {436, 226}, {573, 226}, {573, 52}, {441, 52}
+        };
+
+        double totalDistance = 0.0;
+
+        // Calculate the total distance for the path
+        for (int i = 0; i < nodes.length - 1; i++) {
+            totalDistance +=manhattanDistance(nodes[i], nodes[i + 1]);
+        }
+
+        System.out.println("Total distance for the path: " + totalDistance);
     }
-}
+    
+    
+    
+    
+    
+    
+    //https://www.geeksforgeeks.org/a-search-algorithm/
+    
+    
+    
+    
+	    }
+    
