700d773015a10e03a1ef6d15928fffdd1861b40d
marlonbocke <149473222+marlonbocke@users.noreply.github.com>
2024-10-29 19:01:56 +0100

    updated interface Class

--- Krankenhaus2024/src/package01/Interface.java
+++ Krankenhaus2024/src/package01/Interface.java
@@ -1,16 +1,25 @@
 package package01;
 
 import java.awt.EventQueue;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.lang.classfile.components.ClassPrinter.Node;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
+import java.awt.event.MouseMotionAdapter;
+import java.lang.classfile.components.ClassPrinter.LeafNode;
+import java.lang.classfile.components.ClassPrinter.Node;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import javax.lang.model.element.Name;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Interface {
 
@@ -19,14 +28,7 @@ public class Interface {
     private JTextField textfield_destination_room;
     private String image_path = "/package01/hospital_plan.png";
     private JLabel image_hospital_plan;
-    
-   // int parent_i, parent_j;
-    //double f, g, h;
- 
-    
-  
-    
-    
+
     /**
      * Launch the application.
      */
@@ -117,118 +119,109 @@ public class Interface {
         textfield_destination_room.setColumns(10);
         textfield_destination_room.setBounds(20, 221, 109, 20);
         frame.getContentPane().add(textfield_destination_room);
-       
-        
-
-    
-
     }
-    
+
     int[] position; // [x, y] coordinates of the node
     Node parent;    // the previous node in the path
     int g;          // cost from the start node
     int h;          // heuristic cost (Manhattan distance to goal)
     int f;          // total cost (g + h)
 
-    public Node(int[] position, Node parent) {
+
+    public void Node(int[] position, Node parent) {
         this.position = position;
         this.parent = parent;
         this.g = 0;
         this.h = 0;
         this.f = 0;
     }
-    
-    
-    private void calulate() {
-      System.out.println("calulate");
-      
-   int[] startNode = {100, 200};  // coordinates of the start node (point)
-   int[] goalNode = {500, 400};   // coordinates of the destination node (point)
-	
-   double distance = manhattanDistance(startNode, goalNode);
 
-   
-   System.out.println("The distance value are : " + distance );
- }
-    private static double manhattanDistance(int[] startNode, int[] goalNode) {
-    	
-    	
-    	int x12 = startNode[0];
+    private void calulate() {
+        System.out.println("calulate");
+        
+     int[] startNode = {100, 200};  // coordinates of the start node (point)
+     int[] goalNode = {500, 400};   // coordinates of the destination node (point)
+  	
+     double distance = manhattanDistance(startNode, goalNode);
+
+     
+     System.out.println("The distance value are : " + distance );
+   }
+
+	private static double manhattanDistance(int[] startNode, int[] goalNode) {
+		
+		
+		int x12 = startNode[0];
         int y12 = startNode[1];
         int x13 = goalNode[0];
         int y13 = goalNode[1];
 
-        
+
         int result=(int) Math.sqrt(Math.pow(x13 - x12, 2) + Math.pow(y13 - y12, 2));
         return result;
-			}
 
-    public static void distance(String[] arg) {
-        // Array of node coordinates
-        int[][] nodes = {
+		
+	}
 
-            {64, 47}, {64, 226}, {204, 47}, {204, 226}, {252, 226},
-            {252, 395}, {436, 395}, {436, 226}, {573, 226}, {573, 52}, {441, 52}
-        };
+	 public static void distance(String[] arg) {
+	        // Array of node coordinates
+	        int[][] nodes = {
 
-        double totalDistance = 0.0;
+	            {64, 47}, {64, 226}, {204, 47}, {204, 226}, {252, 226},
+	            {252, 395}, {436, 395}, {436, 226}, {573, 226}, {573, 52}, {441, 52}
+	        };
 
-        // Calculate the total distance for the path
-        for (int i = 0; i < nodes.length - 1; i++) {
-            totalDistance +=manhattanDistance(nodes[i], nodes[i + 1]);
-        }
+	
+	
+	  double totalDistance = 0.0;
 
-        System.out.println("Total distance for the path: " + totalDistance);
-    }
+	        // Calculate the total distance for the path
+	        for (int i = 0; i < nodes.length - 1; i++) {
+	            totalDistance +=manhattanDistance(nodes[i], nodes[i + 1]);
+	        }
+
+	        System.out.println("Total distance for the path: " + totalDistance);
+	    }
+     
+	 public static List<Node> aStarSearch(int[] start, int[] goal, int[][] grid) {
+		 
+		 PriorityQueue<Node> openList = new PriorityQueue<>((a, b) -> a.f - b.f); // open list
+	        List<Node> closedList = new ArrayList<>(); // closed list
+
+		 
+		 
+		 
+		return null;
     
+	
+		 
+		 
+	
     
- 
-   // A* Search Algorithm
-    public static List<Node> aStarSearch(int[] start, int[] goal, int[][] grid) {
-        PriorityQueue<Node> openList = new PriorityQueue<>((a, b) -> a.f - b.f); // open list
-        List<Node> closedList = new ArrayList<>(); // closed list
-
-        Node startNode = new Node(start, null);
-        Node goalNode = new Node(goal, null);
-
-        openList.add(startNode);
-
-        while (!openList.isEmpty()) {
-            Node current = openList.poll();
-
-            if (Arrays.equals(current.position, goal)) {
-                return reconstructPath(current);  // Found goal, return the path
-            }
-
-            closedList.add(current);
-
-            for (Node neighbor : getNeighbors(current, grid)) {
-                if (closedList.contains(neighbor)) continue;
-
-                int tentativeG = current.g + 1; // Assuming each step costs 1
-
-                if (!openList.contains(neighbor) || tentativeG < neighbor.g) {
-                    neighbor.g = tentativeG;
-                    neighbor.h = manhattanDistance(neighbor.position, goal);
-                    neighbor.f = neighbor.g + neighbor.h;
-                    neighbor.parent = current;
-
-                    if (!openList.contains(neighbor)) {
-                        openList.add(neighbor);
-                    }
-                }
-            }
+	 }
     
-    
-    
-    
-    
-    
-    //https://www.geeksforgeeks.org/a-search-algorithm/
-    
-    
-    
-    
-	 //   }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 }
-
