2fc405baec9e2e52535d4bf425502ff90bb17da9
marlonbocke <149473222+marlonbocke@users.noreply.github.com>
2024-12-18 17:29:55 +0100

    Update Main_window.java

--- Software_2024_2/Krankenhaus2024/src/package01/Main_window.java
+++ Software_2024_2/Krankenhaus2024/src/package01/Main_window.java
@@ -10,13 +10,25 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.Font;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class Main_window {
 
@@ -42,6 +54,11 @@ public class Main_window {
     private JLabel current_room;   
     private JLabel destination_room;
     
+    JButton button_commit;
+    
+     private JLabel label_4;
+     private JLabel label_5;
+    
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -61,24 +78,24 @@ public class Main_window {
 
     public void initialize(int[][] positionsArray) {
         frame = new JFrame();
-        frame.setBounds(100, 100, 1048, 707);
+        frame.setBounds(100, 100, 1191, 707);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
 
         drawingPanel = new DrawingPanel();
         drawingPanel.setPositionsArray(positionsArray);
         drawingPanel.setOpaque(false);
-        drawingPanel.setBounds(342, 93, 657, 464); 
+        drawingPanel.setBounds(494, 93, 657, 464); 
         frame.getContentPane().add(drawingPanel);
 
         
     	current_room = new JLabel("Startpunkt");
-        current_room.setBounds(98, 186, 182, 21);
+        current_room.setBounds(283, 237, 182, 21);
         frame.getContentPane().add(current_room);
 
         
         destination_room = new JLabel("Zielpunkt------------------");
-        destination_room.setBounds(109, 217, 171, 21);
+        destination_room.setBounds(283, 268, 171, 21);
         frame.getContentPane().add(destination_room);
 
 
@@ -98,23 +115,16 @@ public class Main_window {
                 
                 int[][] positionsArray = Node.start(start, goal);
                 
-//                for(int i=0; i < positionsArray.length; i++)
-//    		        {
-//    		        	
-//    		        	for(int j=0; j < positionsArray[i].length; j++)
-//    		            {
-//    		            	
-//    		        		System.out.println("Main_window:"+positionsArray[i][j]);
-//    		                        	      	
-//    		            }	
-
-    	        	
-//    		        }              
-                
-                drawingPanel.setPositionsArray(positionsArray);
+                if (positionsArray == null) {
+                    JOptionPane.showMessageDialog(frame, "Kein Pfad gefunden");
+                } else {
+                    // Erfolgreiches Ergebnis verarbeiten
+                    drawingPanel.setPositionsArray(positionsArray);
+                } 
+               // drawingPanel.setPositionsArray(positionsArray);
             }
         });
-        button_start_search.setBounds(198, 410, 121, 40);
+        button_start_search.setBounds(333, 412, 121, 40);
         frame.getContentPane().add(button_start_search);
         button_start_search.setVisible(false);
       
@@ -130,7 +140,7 @@ public class Main_window {
         
         image_hospital_plan = new JLabel("hospital_plan");
         image_hospital_plan.setIcon(new ImageIcon(Main_window.class.getResource("/package01/hospital_plan.png")));
-        image_hospital_plan.setBounds(342, 93, 657, 464);
+        image_hospital_plan.setBounds(494, 93, 657, 464);
         frame.getContentPane().add(image_hospital_plan);
 
        
@@ -144,18 +154,18 @@ public class Main_window {
        
         textfield_destination_room = new JTextField();
         textfield_destination_room.setColumns(10);
-        textfield_destination_room.setBounds(196, 351, 109, 20);
+        textfield_destination_room.setBounds(345, 322, 109, 29);
         frame.getContentPane().add(textfield_destination_room);
         
         JLabel lblNewLabel = new JLabel("HospitalMaps");
         lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 31));
-        lblNewLabel.setBounds(590, 24, 187, 56);
+        lblNewLabel.setBounds(728, 27, 187, 56);
         frame.getContentPane().add(lblNewLabel);
 
         
         JLabel pointLabel = new JLabel();
         pointLabel.setOpaque(true);
-        pointLabel.setBackground(Color.RED); // Punktfarbe
+        pointLabel.setBackground(Color.BLUE); // Punktfarbe
         pointLabel.setSize(10, 10); // Punktgröße
         pointLabel.setVisible(false);
         image_hospital_plan.add(pointLabel);
@@ -170,37 +180,116 @@ public class Main_window {
         pointLabel2.setVisible(false);
         image_hospital_plan.add(pointLabel2);
         
+        label_4 = new JLabel("Bitte geben sie den Ziel Raum ein");
+        label_4.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        label_4.setBounds(100, 314, 235, 40);
+        frame.getContentPane().add(label_4);
         
         
+        label_5 = new JLabel("Bitte markieren die eine Startposition ");
+        label_5.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        label_5.setBounds(213, 167, 252, 51);
+        frame.getContentPane().add(label_5);
+       
+        
+        
+        button_commit = new JButton("Bestätigen");
+        button_commit.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		
+        		String room_id= textfield_destination_room.getText();
+            	try (Connection conn = DatabaseManager.getConnection()) {
+                    String query = "SELECT x_coordinate, y_coordinate FROM room_coordinates WHERE room_id = ?";
+                    PreparedStatement stmt = conn.prepareStatement(query);
+                   
+					stmt.setString(1, room_id);
+                    
+                    ResultSet rs = stmt.executeQuery();
+
+                    if (rs.next()) {
+                    	get_gX = (int) rs.getDouble("x_coordinate");
+                    	get_gY = (int) rs.getDouble("y_coordinate");
+                    	
+                    	System.out.println("X-Koordinate: " + get_gX);
+                        System.out.println("Y-Koordinate: " + get_gY);
+                    	
+                        button_start_search.setVisible(true);
+                  	  destination_room.setVisible(true);
+                  	  destination_room.setText("Zielpunkt gesetzt: "+get_gX+""+get_gY);           	
+                  	  pointLabel2.setLocation(get_gX - pointLabel2.getWidth() / 2, get_gY - pointLabel2.getHeight() / 2);
+                        pointLabel2.setVisible(true);
+                   	
+                    }
+                    	else {
+                    		textfield_destination_room.setText("");
+                    		JOptionPane.showMessageDialog(frame, "Bitte eine gültige Raumnummer von 1-144 eingeben!");
+                    		get_gX=0;
+                    		get_gY=0;
+                    		
+                    	}
+            	
+                    } catch (SQLException e1) {
+        				// TODO Auto-generated catch block
+        				e1.printStackTrace();
+        				System.out.println("Something went wrong"+e1);
+        				JOptionPane.showMessageDialog(frame, "Database error: " + e1.getMessage());
+        				JOptionPane.showMessageDialog(frame, "Please activate  your wifi connection!!");
+        			}
+  	
+            	}
+                    
+                                       
+                    
+        });
+        button_commit.setBounds(333, 361, 121, 40);
+        frame.getContentPane().add(button_commit);
+
+        
+
+        
+
+        textfield_destination_room.setVisible(false);
+        label_4.setVisible(false);
+        button_commit.setVisible(false); 
        
                
         image_hospital_plan.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
             	
             	if(isSetstart)
-            	{
+            	{   
+            		get_sX = e.getX();
+                    get_sY = e.getY();
+                                                			  
+                  
+                     System.out.println("Maus bewegt: X = " + get_sX + ", Y = " + get_sY);
+                      	 current_room.setVisible(true);
+                      	current_room.setText("Akuelle Postion: "+get_sX+""+get_sY);
+                      	
+                       
+                    
+            		
             		// System.out.println("isSetstart true");
-            		  get_sX = e.getX();
-                      get_sY = e.getY();
-                 	System.out.println("Maus bewegt: X = " + get_sX + ", Y = " + get_sY);
-                 	 current_room.setVisible(true);
-                 	current_room.setText("Startpunkt gesetzt: "+get_sX+""+get_sY);
-                 
+            		  
                  	// current_room = new JLabel("Aktuelle Position:"+ get_sX +""+get_sY);            	
                  	//current_room .setVisible(true);
                  	
                  	  pointLabel.setLocation(get_sX - pointLabel.getWidth() / 2, get_sY - pointLabel.getHeight() / 2);
                       pointLabel.setVisible(true);
-                 	
+                      textfield_destination_room.setVisible(true);
+                      label_4.setVisible(true);
+                      label_5.setVisible(false);
+                      button_commit.setVisible(true); 
+                      
             		 isSetstart = false;
-                   	            			
+            		 
             	} else {
             		            		
             	get_gX = e.getX();
             	get_gY = e.getY();
             	
             	
-            	
+            	            	           	
             	
             	System.out.println("Maus bewegt: X = " + get_gX + ", Y = " + get_gY);
             	button_start_search.setVisible(true);
@@ -213,13 +302,10 @@ public class Main_window {
                  	  
             }    
             	
-            	
+                        	
             }
         });
-
-        
-     	
-     	
+    	
      	//current_room.setText(get_sX+""+get_sY);
         
         
@@ -234,10 +320,16 @@ public class Main_window {
                 button_start_search.setVisible(false);
                 pointLabel2.setVisible(false);
                 pointLabel.setVisible(false);
+                textfield_destination_room.setVisible(false);
+                label_4.setVisible(false);
+                destination_room.setVisible(false);
+                current_room.setVisible(false);
+                label_5.setVisible(true);
+                button_commit.setVisible(false);
         		drawingPanel.setPositionsArray(positionsArray);
         	}
         });
-        button_reset.setBounds(198, 461, 121, 40);
+        button_reset.setBounds(333, 462, 121, 40);
         frame.getContentPane().add(button_reset);
 
         JButton btnConfig = new JButton("Config");
@@ -247,17 +339,11 @@ public class Main_window {
         		
         	}
         });
-        btnConfig.setBounds(198, 512, 121, 45);
+        btnConfig.setBounds(333, 512, 121, 45);
         frame.getContentPane().add(btnConfig);
-
-                
         
-   
-       
+                     
     }
-
-    
-    
 }
     
     
